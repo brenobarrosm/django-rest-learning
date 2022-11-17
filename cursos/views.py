7f8a3dde8a38a03630096907f26581bb9a29b8ce
Breno de Barros <brenowbarros21@gmail.com>
2022-11-17 10:24:45 -0400

    Implementando métodos POST

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.views import APIView, status
 
 from .models import Avaliacao, Curso
 from .serializers import AvaliacaoSerializer, CursoSerializer
@@ -12,6 +12,12 @@ class CursoAPIView(APIView):
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class AvaliacaoAPIView(APIView):
 
@@ -19,3 +25,9 @@ class AvaliacaoAPIView(APIView):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
