fa93a05fb0f04299690316c1af9bbb89b9091edf
Breno de Barros <brenowbarros21@gmail.com>
2022-11-19 11:09:16 -0400

    Sobreescrevendo métodos genéricos do DRF

--- cursos/urls.py
+++ cursos/urls.py
@@ -6,7 +6,10 @@ from .views import (AvaliacaoAPIView, AvaliacoesAPIView, CursoAPIView,
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='curso_a valiacao'),
+
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao')
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao')
 ]
  
